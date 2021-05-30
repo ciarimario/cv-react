f6d11010bde33f2f8aad78de9d780af57d110c22
mario ciari <mario.ciari@yahoo.fr>
2021-05-30 18:44:44 +0200

    Navigation et styles

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navigation from '../components/Navigation'
 
 const Portfolio = () => {
     return (
         <div>
-            <h1>Portfolio</h1>
+            <Navigation />
         </div>
     );
 };
